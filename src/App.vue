3ea775c9ccf9275ebdca6bfb394c9e83594834be
damienaries <damien.aries@gmail.com>
2023-03-27 20:57:17 -0700

    firebase spark initial setup

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,31 @@
 </template>
 
 <script setup>
+import { collection, addDoc } from 'firebase/firestore'
+import db from './firebase/init.js'
 import Layout from './components/Layout.vue'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  createUser();
+})
+
+const createUser = async () => {
+  // 'users' collection reference
+  const colRef = collection(db, 'users');
+
+  // data to send
+  const dataObj = {
+    firstName: 'john',
+    lastname: 'doe',
+    dob: '1990'
+  }
+
+  // create document and return reference to it
+  const docRef = await addDoc(colRef, dataObj);
+
+  // Access auto-generated id at '.id'
+  console.log('document created with id:', docRef.id)
+}
 
 </script>
---
+++ src/firebase/init.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCxU-_TF9VsXOUNb92SftcJtOdLsnfh5vY",
+  authDomain: "drink-checker.firebaseapp.com",
+  projectId: "drink-checker",
+  storageBucket: "drink-checker.appspot.com",
+  messagingSenderId: "150203844548",
+  appId: "1:150203844548:web:c8384c4a3fd1f2c0c14691",
+  measurementId: "G-FEYN1H6ZF0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const db = getFirestore();
+export default db;
