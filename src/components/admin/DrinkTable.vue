aa7489f164c51004ad614f3f1766f0b60ceee638
damienaries <damien.aries@gmail.com>
2024-11-07 22:48:58 -0800

    drink table styling

--- src/components/admin/DrinkTable.vue
+++ src/components/admin/DrinkTable.vue
@@ -1,38 +1,46 @@
 <template>
-  <table v-if="drinks" class="w-full lg:w-11/12 my-8 mx-auto bg-gray-100 shadow-lg rounded p-4">
-    <tr>
-      <th>Name</th>
-      <th>Ingredients</th>
-      <th>Method</th>
-      <th>Glass</th>
-      <th>Garnish</th>
-      <th>Ice</th>
-      <th></th>
-    </tr>
-    <tr v-for="drink in drinks" :key="drink.id">
-      <td class="table-cell">{{ drink.name }}</td>
-      <td class="table-cell" >
-        <tr v-for="ing in drink.ingredients" 
-          :key="ing.name"
-          class="w-full flex items-center justify-between border-b border-gray-200">
-          <td class="flex-1">{{ ing.name }}</td>
-          <td class="w-1/3">{{ ing.quantity }} <span v-if="!isNaN(ing.quantity)">Oz</span></td>
+  <section v-if="drinks" class="container-margin drink-table-container">
+    <div class="mb-2 shadow-sm" v-for="drink in drinks" :key="drink.id">
+      <div class="flex justify-between items-center bg-gray-100 border-b border-gray-200 p-4">
+        <span @click="toggleCollapse(drink.id)" :class="{'rotate-90': expanded === drink.id}">></span>
+        <h5>{{ drink.name }}</h5>
+        <div class="flex space-x-4">
+          <button-component @click.prevent="drinkToEdit = drink">
+            <svg-icon icon="edit" color="#111" size="18"></svg-icon>
+          </button-component>
+          <button-component @click.prevent="deleteDrink(drink.id)" color="danger">
+            <svg-icon icon="delete" color="#fff" size="18"></svg-icon>
+          </button-component>
+        </div>
+      </div>
+
+      <table v-if="expanded === drink.id" class="w-full rounded p-4">
+        <tr class="bg-gray-200">
+          <th>Ingredients</th>
+          <th>Method</th>
+          <th>Glass</th>
+          <th>Garnish</th>
+          <th>Ice</th>
         </tr>
-      </td>
-     <td class="table-cell">{{ drink.method }}</td>
-     <td class="table-cell">{{ drink.glass }}</td>
-     <td class="table-cell">{{ drink.garnish }}</td>
-     <td class="table-cell">{{ drink.ice }}</td>
-     <td class="table-cell space-x-4">
-      <button-component @click.prevent="drinkToEdit = drink">
-        <svg-icon icon="edit" color="#111" size="18"></svg-icon>
-      </button-component>
-      <button-component @click.prevent="deleteDrink(drink.id)" color="danger">
-        <svg-icon icon="delete" color="#fff" size="18"></svg-icon>
-      </button-component>
-     </td>
-    </tr>
-  </table>
+        <tr v-for="drink in drinks" :key="drink.id">
+          <td class="table-cell" >
+            <tr v-for="ing in drink.ingredients" 
+              :key="ing.name"
+              class="w-full flex items-center justify-between">
+              <td class="flex-1">{{ ing.name }}</td>
+              <td class="w-1/3">{{ ing.quantity }} <span v-if="!isNaN(ing.quantity)">Oz</span></td>
+            </tr>
+          </td>
+        <td class="table-cell">{{ drink.method }}</td>
+        <td class="table-cell">{{ drink.glass }}</td>
+        <td class="table-cell">{{ drink.garnish }}</td>
+        <td class="table-cell">{{ drink.ice }}</td>
+        
+        </tr>
+      </table>
+    </div>
+  </section>
+
 
   <edit-drink-modal 
      v-if="drinkToEdit"
@@ -60,6 +68,11 @@ onMounted(() => {
   });
 })
 
+const expanded = ref(null);
+
+const toggleCollapse = (id) => {
+  expanded.value = expanded.value? null : id;
+}
 
 </script>
 
@@ -69,5 +82,16 @@ onMounted(() => {
   padding: .5rem 1rem;
   background: #fff;
   text-align: left;
+  }
+  
+  /* todo make global container, add to page level and create grid or base layout */
+  .container-margin {
+    margin: 0 100px;
+  }
+
+  @media screen and (max-width: 800px) {
+    .container-margin {
+      margin: 0 50px;
+    }
   }
 </style>
